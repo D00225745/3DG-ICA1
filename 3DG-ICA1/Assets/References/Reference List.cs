f489450a24aa4ab5e0348a2f3a8ffa222d4322c8
D00225745 <D00225745@student.dkit.ie>
2022-12-29 15:39:48 +0000

    Updated Nav Mesh Agent
    Updated the stats on the Nav Mesh Agent for a smoother movement and turns for the player character

---
+++ 3DG-ICA1/Assets/References/Reference List.cs
@@ -0,0 +1,11 @@
+/*
+ * A cheeky way to add a list of references IN the game assignment.
+ * 
+ * https://www.mixamo.com/#/?page=1&type=Character
+ * 
+ * For the demo character model
+ * https://assetstore.unity.com/packages/templates/systems/3rd-person-controller-fly-mode-28647
+ * 
+ * For platforms that are moving
+ * https://www.youtube.com/watch?v=dWtjKKR7Q3s
+ */
