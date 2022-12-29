180b1730fed973656a7a014745f8331d5bcdd825
D00225745 <D00225745@student.dkit.ie>
2022-12-29 16:06:53 +0000

    Added New Code and Animations
    Added new code for player movement, and tried adding walk and idle animations. Perhaps the new movement code may allow the previous jump code to work properly

--- 3DG-ICA1/Assets/References/Reference List.cs
+++ 3DG-ICA1/Assets/References/Reference List.cs
@@ -6,6 +6,8 @@
  * For the demo character model
  * https://assetstore.unity.com/packages/templates/systems/3rd-person-controller-fly-mode-28647
  * 
+ * 
+ * 
  * For platforms that are moving
  * https://www.youtube.com/watch?v=dWtjKKR7Q3s
  */
