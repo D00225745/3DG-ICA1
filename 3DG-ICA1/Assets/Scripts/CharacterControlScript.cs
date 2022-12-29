b8a09b19c0b7d05fc26d0a235e2c3bf52222fd09
D00225745 <D00225745@student.dkit.ie>
2022-12-29 16:38:18 +0000

    Added Animations
    Animations of walking and idle works properly. But once the character arrives at their destination, they keep walking circles.

--- 3DG-ICA1/Assets/Scripts/CharacterControlScript.cs
+++ 3DG-ICA1/Assets/Scripts/CharacterControlScript.cs
@@ -5,19 +5,17 @@ public class CharacterControlScript : MonoBehaviour
     public Camera cam;
     public NavMeshAgent player;
     public Animator playerAnimator;
-    public GameObject targetDest;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitPoint;
 
             if(Physics.Raycast(ray, out hitPoint))
             {
-                targetDest.transform.position = hitPoint.point;
                 player.SetDestination(hitPoint.point);
             }
         }
