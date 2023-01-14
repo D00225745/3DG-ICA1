7daa0177db378cce770775cb170f8a7f361bc10d
D00225745 <D00225745@student.dkit.ie>
2023-01-14 13:18:19 +0000

    Attempted Moving Platforms
    Found out I require NavMeshComponents for that

---
+++ 3DG-ICA1/Assets/Scripts/MovingPlatform/MovingPlatform.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] Positions;
+    [SerializeField]
+    private float DockDuration = 2f;
+    [SerializeField]
+    private float MoveSpeed = 0.01f;
+
+    private List<NavMeshAgent> AgentsOnPlatform = new List<NavMeshAgent>();
+
+    private void Start()
+    {
+        StartCoroutine(MovePlatform());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<NavMeshAgent>(out NavMeshAgent agent))
+        {
+            AgentsOnPlatform.Add(agent);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<NavMeshAgent>(out NavMeshAgent agent))
+        {
+            AgentsOnPlatform.Remove(agent);
+        }
+    }
+
+    private IEnumerator MovePlatform()
+    {
+        transform.position = Positions[0];
+        int positionIndex = 0;
+        int lastPositionIndex;
+        WaitForSeconds Wait = new WaitForSeconds(DockDuration);
+
+        while(true)
+        {
+            lastPositionIndex = positionIndex;
+            positionIndex++;
+            if(positionIndex >= Positions.Length)
+            {
+                positionIndex = 0;
+            }
+
+            Vector3 platformMoveDirection = (Positions[positionIndex] - Positions[lastPositionIndex]).normalized;
+            float distance = Vector3.Distance(transform.position, Positions[positionIndex]);
+            float distanceTraveled = 0;
+
+            while(distanceTraveled < distance)
+            {
+                transform.position += platformMoveDirection * MoveSpeed;
+                distance += platformMoveDirection.magnitude * MoveSpeed;
+
+                for(int i = 0; i < AgentsOnPlatform.Count; i++)
+                {
+                    AgentsOnPlatform[i].Warp(AgentsOnPlatform[i].transform.position + platformMoveDirection * MoveSpeed);
+                }
+
+                yield return null;
+            }
+
+            yield return Wait;
+        }
+    }
+
+}
